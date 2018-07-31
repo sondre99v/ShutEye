259a95e93b4ea9153a5a10914a9f6e962e20f9e8
Sondre Ninive Andersen <sondreixix@gmail.com>
2018-07-31 15:56:12 +0200

    Rename PsgChannel to Timeseries, and change back to using floats

--- ShutEye/Polysomnogram.cs
+++ ShutEye/Polysomnogram.cs
@@ -13,18 +13,19 @@ namespace ShutEye
 
         public int SamplesPrChannel { get => (int) (SampleRate * Duration); }
 
-        public PsgChannel[] Channels;
+        public Timeseries[] Channels;
 
         public Polysomnogram(EDFFile edfFile)
         {
             // Load file
-            Channels = new PsgChannel[edfFile.Header.NumberOfSignalsInDataRecord];
+            Channels = new Timeseries[edfFile.Header.NumberOfSignalsInDataRecord];
             SampleRate = edfFile.Header.Signals[0].NumberOfSamplesPerDataRecord / edfFile.Header.DurationOfDataRecordInSeconds;
             Duration = edfFile.Header.DurationOfDataRecordInSeconds * edfFile.Header.NumberOfDataRecords;
 
             for(int i = 0; i < Channels.Length; i++)
             {
-                Channels[i] = new PsgChannel(edfFile, i);
+                Channels[i] = new Timeseries();
+                Channels[i].LoadFromEdfFile(edfFile, i);
             }
         }
     }
---
+++ ShutEye/Timeseries.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EDF;
+
+namespace ShutEye
+{
+    class Timeseries
+    {
+        public string Label { get; private set; }
+        public float SampleRate { get; private set; }
+
+        public float[] Data;
+
+        public void LoadFromEdfFile(EDFFile file, int channelIndex)
+        {
+            Label = file.Header.Signals[channelIndex].Label;
+            SampleRate = file.Header.Signals[channelIndex].NumberOfSamplesPerDataRecord / file.Header.DurationOfDataRecordInSeconds;
+
+            int numberOfSamples = file.Header.Signals[channelIndex].NumberOfSamplesPerDataRecord * file.Header.NumberOfDataRecords;
+
+            Data = new float[numberOfSamples];
+
+            for(int i = 0; i < Data.Length; i++)
+            {
+                EDFDataRecord record = file.DataRecords[i / file.Header.Signals[channelIndex].NumberOfSamplesPerDataRecord];
+                List<float> recordData = record[channelIndex + 1];
+
+                Data[i] = recordData[i % file.Header.Signals[channelIndex].NumberOfSamplesPerDataRecord];
+            }
+        }
+    }
+}
